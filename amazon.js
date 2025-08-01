ecf1205e296ca9ee6cc6424bb47d06a41f839b07
Banana-89 <157877712+Banana-89@users.noreply.github.com>
2025-08-01 17:41:47 +0700

    new

--- amazon.js
+++ amazon.js
@@ -28,7 +28,7 @@ products.forEach((product) => {
 					<div class="product-price">${(product.priceCents / 100).toFixed(2) + "$"}</div>
 
 					<div class="product-quantity-container">
-						<select>
+						<select class= "js-quantity-selector-${product.id}">
 							<option selected value="1">1</option>
 							<option value="2">2</option>
 							<option value="3">3</option>
@@ -44,14 +44,15 @@ products.forEach((product) => {
 
 					<div class="product-spacer"></div>
 
-					<div class="added-to-cart " >
+					<div class="added-to-cart js-added-to-cart " >
 						<img src="images/icons/checkmark.png" />
 						Added
 					</div>
 
 					<button class="add-to-cart-button button-primary js-addToCartBtn"  
-					data-product-name =" ${product.name}"
-					data-product-Id="${product.id}">Add to Cart</button>
+					data-product-name =" ${product.name}
+					"
+					data-product-id="${product.id}">Add to Cart</button>
 				</div>
 			</div>
     `;
@@ -61,27 +62,53 @@ products.forEach((product) => {
 
 //.querySelectorAll return nodelist
 let addToCartBtn = document.querySelectorAll(".js-addToCartBtn");
-
+//What will happen inside each button?
 addToCartBtn.forEach((button) => {
 	button.onclick = () => {
 		const productName = button.dataset.productName;
+		const productId = button.dataset.productId;
+
+		// Get current product card in the button
+		const productCard = button.closest(".product-container");
+
+		//Added to cart showed up after the button is clicked
+		const addedBtn = productCard.querySelector(".js-added-to-cart");
+		addedBtn.classList.add("js-added");
+		setTimeout(() => {
+			addedBtn.classList.remove("js-added");
+		}, 2000);
+		// Selector for product quantity
+
+		let selectorInput = document.querySelector(
+			`.js-quantity-selector-${productId}`
+		);
+		let selector = Number(selectorInput.value);
+
+		//Condition to prevent product duplication
 
 		let matchingItem;
 		carts.forEach((item) => {
-			if (item.name === productName) {
+			if (item.id === productId) {
 				matchingItem = item;
 			}
 		});
 		if (matchingItem) {
-			matchingItem.quantity += 1;
+			matchingItem.quantity += selector;
 		} else {
 			carts.push({
 				name: productName,
-				quantity: 1,
+				id: productId,
+				quantity: selector,
 			});
 		}
 
-		alert(productName);
-		console.log(carts);
+		//Calculation of the total quantity selected
+		let totalQuantity = 0;
+
+		carts.forEach((item) => {
+			totalQuantity += item.quantity;
+		});
+
+		document.querySelector(".js-cart-quantity").innerHTML = totalQuantity;
 	};
 });
